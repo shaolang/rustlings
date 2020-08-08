d485c033dc0c566735ed80e6f80f14f1a30d6645
shaolang <shaolang@ymail.com>
2020-08-08 21:34:27 +0800

    Complete primitive types exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,11 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn ..... {
+fn calculate_apple_price(n: u32) -> u32 {
+    n * if n <= 40 { 2 } else { 1 }
+}
 
 // Don't modify this function!
 #[test]
